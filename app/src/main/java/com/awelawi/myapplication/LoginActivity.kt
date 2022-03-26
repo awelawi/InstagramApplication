9265e2a75916dd84c1d8605add360b3b2d7d5b2f
Ella <emmanuellaumoye18@gmail.com>
2022-03-26 11:38:13 -0400

    commit file

--- app/src/main/java/com/awelawi/myapplication/LoginActivity.kt
+++ app/src/main/java/com/awelawi/myapplication/LoginActivity.kt
@@ -13,18 +13,52 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //Check if user is logged in
+        //If true, take them to MainActivity
 
+        if(ParseUser.getCurrentUser() != null){
+            goToMainActivity()
+        }
         findViewById<Button>(R.id.LoginButton).setOnClickListener {
             val username = findViewById<EditText>(R.id.etUserName).text.toString()
             val password = findViewById<EditText>(R.id.etPassWord).text.toString()
             loginUser(username, password)
+        }
+        findViewById<Button>(R.id.signUp).setOnClickListener {
+            val username = findViewById<EditText>(R.id.etUserName).text.toString()
+            val password = findViewById<EditText>(R.id.etPassWord).text.toString()
+            signUpUser(username, password)
+        }
+    }
+    private fun signUpUser(username: String, password: String){
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (user != null) {
+                // Hooray! Let them use the app now.
+                //Navigate the user to main activity
+                Toast.makeText(this, "successfully login the user", Toast.LENGTH_SHORT).show()
+                goToMainActivity()
+
+                //Make a toast to show the user signup successfully
+            } else {
+                // Sign up didn't succeed. Look at the ParseException
+                // to figure out what went wrong
+                //Show a toast to the user to tell them failed sign up
+                e.printStackTrace()
+            }
         }
     }
     private fun loginUser(username: String, password: String){
         ParseUser.logInInBackground(username, password, ({ user, e ->
             if (user != null) {
                 // Hooray!  The user is logged in.
-                Toast.makeText(this, "succesfully logged in user", Toast.LENGTH_SHORT).show()
+                Log.i(TAG, "Succesfully logged user")
                 goToMainActivity()
             } else {
                 // Signup failed.  Look at the ParseException to see what happened.
@@ -38,6 +72,8 @@ class LoginActivity : AppCompatActivity() {
     private fun goToMainActivity(){
         val intent = Intent(this@LoginActivity, MainActivity::class.java)
         startActivity(intent)
+        finish()
+
     }
     companion object{
         const val TAG = "LoginActivity"
